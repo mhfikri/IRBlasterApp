a8d5f8bba631f74ece2cce6c9d85e30d222fe1f8
Fikri <muhammad.fikri@finexusgroup.com>
2023-04-26 10:46:29 +0700

    Redesign IR Blaster recycler view.

--- app/src/main/java/com/example/irblaster/IRBlasterHolder.java
+++ app/src/main/java/com/example/irblaster/IRBlasterHolder.java
@@ -1,7 +1,6 @@
 package com.example.irblaster;
 
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,22 +9,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class IRBlasterHolder extends RecyclerView.ViewHolder {
     private final TextView mDeviceName;
-    private final TextView mSensorTemperature;
-    private final TextView mSensorHumidity;
-    private final ImageView mTemperatureIcon;
+    private final TextView mTemperature;
+    private final TextView mHumidity;
 
     public IRBlasterHolder(@NonNull View itemView) {
         super(itemView);
         mDeviceName = itemView.findViewById(R.id.text_device_name);
-        mSensorTemperature = itemView.findViewById(R.id.text_sensor_temperature);
-        mSensorHumidity = itemView.findViewById(R.id.text_sensor_humidity);
-        mTemperatureIcon = itemView.findViewById(R.id.temperature_icon);
+        mTemperature = itemView.findViewById(R.id.temperature_text);
+        mHumidity = itemView.findViewById(R.id.humidity_text);
     }
 
     public void bind(@NonNull AbstractIRBlaster irBlaster) {
         setName(irBlaster.getName());
         setTemperature(String.format("%1$s°C", irBlaster.getTemperature()));
-        setTemperatureIcon(irBlaster.getTemperature());
         setHumidity(String.format("%1$s%%", irBlaster.getHumidity()));
     }
 
@@ -33,19 +29,11 @@ public class IRBlasterHolder extends RecyclerView.ViewHolder {
         mDeviceName.setText(name);
     }
 
-    private void setTemperature(@Nullable String formattedTemperature) {
-        mSensorTemperature.setText(formattedTemperature);
-    }
-
-    private void setTemperatureIcon(@Nullable String temperature) {
-        if (temperature != null && Double.parseDouble(temperature) <= 26.0) {
-            mTemperatureIcon.setImageResource(R.drawable.ic_temperature_cool_24dp);
-        } else {
-            mTemperatureIcon.setImageResource(R.drawable.ic_temperature_hot_24dp);
-        }
+    private void setTemperature(@Nullable String temperature) {
+        mTemperature.setText(temperature);
     }
 
     private void setHumidity(@Nullable String humidity) {
-        mSensorHumidity.setText(humidity);
+        mHumidity.setText(humidity);
     }
 }
